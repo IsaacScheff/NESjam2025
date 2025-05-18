1fac13f95f5ea22292476670ac78ff4f08c5a1b9
IsaacScheff <isaac1996@comcast.net>
2025-05-17 17:51:13 -1000

    Fixed height and width and added 3 zoom

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import MainScene from './scenes/MainScene.js';
 
 const config = {
   type: Phaser.AUTO,
-  width: 240,
-  height: 256,
-  scene: [MainScene]
+  width: 256,
+  height: 240,
+  scene: [MainScene],
+  zoom: 3
 };
 
 new Phaser.Game(config);
