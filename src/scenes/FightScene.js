d0af71d69ae7770a5e4900298f306a4e4ca3dfd3
IsaacScheff <isaac1996@comcast.net>
2025-06-05 12:26:18 -0400

    Created basic enemy

--- src/scenes/FightScene.js
+++ src/scenes/FightScene.js
@@ -9,12 +9,18 @@ export default class FightScene extends Phaser.Scene {
         this.jumpForce = -160;
         this.jumpHoldTime = 0;
         this.maxJumpDuration = 150;
+        this.isPlayerInvulnerable = false;
     }
 
     preload() {
         this.load.spritesheet('player', 'assets/sprites/ItsaBowser.png', {
             frameWidth: 33,
             frameHeight: 48
+        });
+        
+        this.load.spritesheet('enemy', 'assets/sprites/ItsaMario.png', {
+            frameWidth: 16,
+            frameHeight: 32
         });
     }
 
@@ -27,6 +33,11 @@ export default class FightScene extends Phaser.Scene {
         this.player = this.physics.add.sprite(128, 120, 'player');
         this.player.setCollideWorldBounds(true, true, true, false);
         this.player.setBounce(0.1);
+
+        this.enemy = this.physics.add.sprite(50, 200, 'enemy');
+        this.enemy.setCollideWorldBounds(true);
+        this.enemy.setBounce(1); // Bounce off walls
+        this.enemy.setVelocityX(50); // Initial movement speed
         
         this.isJumping = false;
         this.playerSpeed = 120;
@@ -34,11 +45,17 @@ export default class FightScene extends Phaser.Scene {
         // Create ground (invisible)
         this.ground = this.add.rectangle(0, 240, 256, 10, 0x000000, 0).setOrigin(0, 0);
         this.physics.add.existing(this.ground, true);
+        
         this.physics.add.collider(this.player, this.ground, () => {
             this.isJumping = false;
             this.isJumpHeld = false;
             this.jumpHoldTime = 0;
         });
+        
+        this.physics.add.collider(this.enemy, this.ground);
+        this.physics.add.overlap(this.enemy, this.player, this.hitPlayer, null, this);
+
+        this.player.setDepth(1);
     }
 
     update() {
@@ -63,6 +80,29 @@ export default class FightScene extends Phaser.Scene {
         if (this.inputHandler.justUp('A') && this.isJumping) {
             this.cancelJump();
         }
+    }
+
+    hitPlayer() {
+        if (this.isPlayerInvulnerable) return;
+        
+        this.isPlayerInvulnerable = true;
+        
+        const flickerTween = this.tweens.add({
+            targets: this.player,
+            alpha: 0,
+            ease: 'Linear',
+            duration: 100,
+            repeat: 2,
+            yoyo: true,
+            onComplete: () => {
+                this.player.setAlpha(1);
+                this.isPlayerInvulnerable = false;
+            }
+        });
+        
+        const knockbackDirection = this.player.x < this.enemy.x ? -1 : 1;
+        this.player.setVelocityX(200 * knockbackDirection);
+        this.player.setVelocityY(-100);
     }
 
     startJump() {
